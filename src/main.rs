a03c2f533498dd0f7f7c10f38a7115c0838b15cd
Jiaming Mao <jiamingm@intel.com>
2020-09-29 10:09:02 +0800

    revised the vec3 functions and added the scene

--- src/main.rs
+++ src/main.rs
@@ -4,76 +4,77 @@ use std::path::Path;
 use std::ops::Add;
 use std::ops::Sub;
 use std::ops::Mul;
+use std::f32::INFINITY;
 
 
-struct Vector(f32, f32, f32);
+struct Vec3(f32, f32, f32);
 
-impl Vector{
-    fn dot(&self, other: &Vector) -> f32{
-        self.x * other.x + self.y * other.y + self.z * other.z
+impl Vec3{
+    fn dot(&self, other: &Vec3) -> f32{
+        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
     }
     fn length(&self) -> f32{
-        1.0 / (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+        1.0 / (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt()
     }
-    fn cross(&self, other: &Vector) -> Vector{
-        Vector{
-            x: self.y * other.z - self.z * other.y,
-            y: self.z * other.x - self.x * other.z,
-            z: self.x * other.z - self.z * other.y
-        }
-    }
-}
-impl Add<&Vector> for Vector{
-    type Output = Vector;
-    fn add(self, other: &Vector) -> Vector{
-        Vector{
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z
-        }
+    fn cross(&self, other: &Vec3) -> Vec3{
+        Vec3(
+            self.1 * other.2 - self.2 * other.1,
+            self.2 * other.0 - self.0 * other.2,
+            self.1 * other.2 - self.2 * other.1
+        )
     }
 }
-impl Sub<&Vector> for Vector{
-    type Output = Vector;
-    fn sub(self, other: &Vector) -> Vector{
-        Vector{
-            x: self.x - other.x,
-            y: self.y - other.y,
-            z: self.z - other.z
-        }
+impl Add<&Vec3> for &Vec3{
+    type Output = Vec3;
+    fn add(self, other: &Vec3) -> Vec3{
+        Vec3(
+            self.0 + other.0,
+            self.1 + other.1,
+            self.2 + other.2
+        )
     }
 }
-impl Mul<f32> for Vector{
-    type Output = Vector;
-    fn mul(self, k: f32) -> Vector{
-        Vector{
-            x: self.x * k,
-            y: self.y * k,
-            z: self.z * k
-        }
+impl Sub<&Vec3> for &Vec3{
+    type Output = Vec3;
+    fn sub(self, other: &Vec3) -> Vec3{
+        Vec3(
+            self.0 - other.0,
+            self.1 - other.1,
+            self.2 - other.2
+        )
     }
 }
-impl Mul<&Vector> for Vector{
-    type Output = Vector;
-    fn mul(self, other: &Vector) -> Vector{
-        Vector{
-            x: self.x * other.x,
-            y: self.y * other.y,
-            z: self.z * other.z
-        }
+impl Mul<f32> for &Vec3{
+    type Output = Vec3;
+    fn mul(self, k: f32) -> Vec3{
+        Vec3(
+            self.0 * k,
+            self.1 * k,
+            self.2 * k
+        )
+    }
+}
+impl Mul<&Vec3> for &Vec3{
+    type Output = Vec3;
+    fn mul(self, other: &Vec3) -> Vec3{
+        Vec3(
+            self.0 * other.0,
+            self.1 * other.1,
+            self.2 * other.2
+        )
     }
 }
 
 struct Ray{
-    origin: Vector,
-    direction: Vector
+    origin: Vec3,
+    direction: Vec3
 }
 
 impl Ray{
-    fn new(origin: &Vector, direction: &Vector) -> Self{
+    fn new(origin: &Vec3, direction: &Vec3) -> Self{
         Ray{
-            origin: Vector::new(origin.x, origin.y, origin.z),
-            direction: Vector::new(direction.x, direction.y, direction.z)
+            origin: Vec3(origin.0, origin.1, origin.2),
+            direction: Vec3(origin.0, origin.1, origin.2)
         }
     }
 }
@@ -86,14 +87,14 @@ enum Refl_t {
 
 struct Sphere{
     radiance: f32,
-    position: Vector,
-    emission: Vector,
-    color: Vector,
+    position: Vec3,
+    emission: Vec3,
+    color: Vec3,
     refl_t: Refl_t
 }
 
 impl Sphere{
-    fn new(radiance: f32, position: Vector, emission: Vector, color: Vector, refl_t: Refl_t) -> Self{
+    fn new(radiance: f32, position: Vec3, emission: Vec3, color: Vec3, refl_t: Refl_t) -> Self{
         Sphere{
             radiance: radiance,
             position: position,
@@ -102,8 +103,8 @@ impl Sphere{
             refl_t: refl_t
         }
     }
-    fn intersect(self, ray: &Ray) -> f32{
-        let op = self.position - &ray.origin;
+    fn intersect(&self, ray: &Ray) -> f32{
+        let op = &self.position - &ray.origin;
         let mut t: f32;
         let eps = 1e-4;
         let b = op.dot(&ray.direction);
@@ -129,11 +130,42 @@ impl Sphere{
     }
 }
 
+fn radiance(scene: &[Sphere], ray: &Ray, depth: i32) -> Vec3{
+    let t = INFINITY;
+    let id: Option<usize>;
+    for i in 0..scene.len(){
+        let d = scene[i].intersect(ray);
+        if d > 1e-20 && d < t{
+            t = d;
+            id = Some(i);
+        }
+    }
+    if let Some(id) = id{
+    }
+    Vec3(0., 0., 0.)
+}
+
 fn main() {
 
     let scene = [
-        Sphere::new(1e5, Vector::new(1e5+1, 40.8, 81.6), Vector::new(0, 0, 0), Vector::new(0.75, 0.75, 0.75), Refl_t::DIFF)),
-        Sphere::new(1e5, Vector::new(-1e5+99, 40.8, 81.6), Vector::new(0, 0, 0), Vector::new(0.25, 0.25, 0.25), Refl_t::DIFF)
+        // Left
+        Sphere::new(1e5, Vec3(1e5 + 1.0, 40.8, 81.6), Vec3(0., 0., 0.), Vec3(0.75, 0.75, 0.75), Refl_t::DIFF),
+        // Right
+        Sphere::new(1e5, Vec3(-1e5 + 99.0, 40.8, 81.6), Vec3(0., 0., 0.), Vec3(0.25, 0.25, 0.25), Refl_t::DIFF),
+        // Back
+        Sphere::new(1e5, Vec3(50.0, 40.8, 1e5), Vec3(0., 0., 0.), Vec3(0.75, 0.75, 0.75), Refl_t::DIFF),
+        // Front
+        Sphere::new(1e5, Vec3(50.0, 40.8, -1e5 + 170.0), Vec3(0., 0., 0.), Vec3(0., 0., 0.), Refl_t::DIFF),
+        // Bottom
+        Sphere::new(1e5, Vec3(50.0, 1e5, 81.6), Vec3(0., 0., 0.), Vec3(0.75, 0.75, 0.75), Refl_t::DIFF),
+        // Top
+        Sphere::new(1e5, Vec3(50.0, -1e5 + 81.6, 81.6), Vec3(0., 0., 0.), Vec3(0.75, 0.75, 0.75), Refl_t::DIFF),
+        // Metal ball
+        Sphere::new(16.5, Vec3(27.0, 16.5, 47.0), Vec3(0., 0., 0.), Vec3(1., 1., 1.) * 0.999, Refl_t::SPEC),
+        // Glass ball
+        Sphere::new(16.5, Vec3(73.0, 16.5, 78.0), Vec3(0., 0., 0.), Vec3(1., 1., 1.) * 0.999, Refl_t::REFR),
+        // Light
+        Sphere::new(600.0, Vec3(50.0, 681.6 - 0.27, 81.6), Vec3(12.0, 12.0, 12.0), Vec3(0., 0., 0.), Refl_t::DIFF),
     ];
 
     let row = 256;
